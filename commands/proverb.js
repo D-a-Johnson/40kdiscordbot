47e6fa8eeb4f6c7d00201150f973cd3ec53ea224
D-a-Johnson <johnson.a.derek.94@gmail.com>
2022-01-25 23:46:57 +0100

    comment

--- commands/proverb.js
+++ commands/proverb.js
@@ -4,7 +4,7 @@ module.exports = {
     async execute(client, message, args){
         const fetch = require('node-fetch');
         let getProverb = async () => {
-            let result = await fetch("http://proverbinatus.com/quotes/random/");
+            let result = await fetch("http://proverbinatus.com/quotes/random/"); //gets the text as a string but statement can be easily adapted to json
             let json = await result.text();
             return json;
           }
